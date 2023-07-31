f0328fdbf43afda547c2f220eb8a78f16c42f212
mariajose2398 <mariajose2398@gmail.com>
2023-07-31 15:44:48 +0000

    Added new histogram : Dark photon decaying to muon with lj nearby

--- sidm/definitions/hists.py
+++ sidm/definitions/hists.py
@@ -699,6 +699,12 @@ hist_defs = {
                    lambda objs, mask: lxy(derived_objs["matched_genAs"](objs, 0.4)) ),
         ],
     ),
+    "matched_genA_toMu_lxy": h.Histogram(
+        [
+            h.Axis(hist.axis.Regular(100, 0, 500, name="matched_genA_toMu_lxy"),
+                   lambda objs, mask: lxy(derived_objs["matched_genAs_toMu"](objs, 0.4)) ),
+        ],
+    ),
     "matched_genA_mu_lxy": h.Histogram(
         [
             h.Axis(hist.axis.Regular(100, 0, 500, name="matched_genA__mu_lxy"),
