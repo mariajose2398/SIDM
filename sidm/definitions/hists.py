ed850556694e53ca459a3e0fbfe851c2d5d9e5b7
mariajose2398 <mariajose2398@gmail.com>
2026-09-14 13:55:02 +0000

    corrected a label

--- sidm/definitions/hists.py
+++ sidm/definitions/hists.py
@@ -5569,7 +5569,7 @@ hist_defs = {
     ),
      "jet0_muLj0_dPhi": h.Histogram(
         [
-            h.Axis(hist.axis.Regular(50, 0, math.pi, name="jet0_muLj0_dPhi", label = r"$\Delta\Phi$ between Jet0 and $\mu$ LJ 0"),
+            h.Axis(hist.axis.Regular(50, 0, math.pi, name="jet0_muLj0_dPhi", label = r"|$\Delta\Phi$| between Jet0 and $\mu$ LJ 0"),
                    lambda objs, mask: abs(objs["jets"][mask, 0].delta_phi(objs["mu_ljs"][mask, 0]))),
         ],
         evt_mask=lambda objs: (ak.num(objs["jets"], axis =1) > 0) & (ak.num(objs["mu_ljs"], axis =1) >0)
